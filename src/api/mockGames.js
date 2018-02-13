fab0be848e0183cd6b119828c446e88526ac8f67
merlingerin <lisg1990@gmail.com>
2018-02-14 00:48:57 +0200

    change url to images

--- src/api/mockGames.js
+++ src/api/mockGames.js
@@ -2,107 +2,107 @@ export default [
     {
         id: 1,
         rate: 4.12,
-        image:  '/uploads/images/games/AnubisSecret.jpg',
+        image:  '/casexe-project/images/games/AnubisSecret.jpg',
         title: 'AnubisSecret'
     }, {
         id: 2,
         rate: 3.11,
-        image: '/uploads/images/games/DreamOfKnight.jpg',
+        image: '/casexe-project/images/games/DreamOfKnight.jpg',
         title: 'Dream of Knight'
     }, {
         id: 3,
         rate: 4.72,
-        image: '/uploads/images/games/LuckyMagic.jpg',
+        image: '/casexe-project/images/games/LuckyMagic.jpg',
         title: 'Lucky Magic'
     }, {
         id: 4,
         rate: 4.02,
-        image: '/uploads/images/games/Submarine.jpg',
+        image: '/casexe-project/images/games/Submarine.jpg',
         title: 'Submarine adventure'
     }, {
         id: 5,
         rate: 3.87,
-        image: '/uploads/images/games/MoneyScent.jpg',
+        image: '/casexe-project/images/games/MoneyScent.jpg',
         title: 'The big money scent'
     }, {
         id: 6,
         rate: 4.12,        
-        image:  '/uploads/images/games/AnubisSecret.jpg',
+        image:  '/casexe-project/images/games/AnubisSecret.jpg',
         title: 'AnubisSecret'
     }, {
         id: 7,
         rate: 3.11,
-        image: '/uploads/images/games/DreamOfKnight.jpg',
+        image: '/casexe-project/images/games/DreamOfKnight.jpg',
         title: 'Dream of Knight'
     }, {
         id: 8,
         rate: 4.72,
-        image: '/uploads/images/games/LuckyMagic.jpg',
+        image: '/casexe-project/images/games/LuckyMagic.jpg',
         title: 'Lucky Magic'
     }, {
         id: 9,
         rate: 4.02,
-        image: '/uploads/images/games/Submarine.jpg',
+        image: '/casexe-project/images/games/Submarine.jpg',
         title: 'Submarine adventure'
     }, {
         id: 10,
         rate: 3.87,
-        image: '/uploads/images/games/MoneyScent.jpg',
+        image: '/casexe-project/images/games/MoneyScent.jpg',
         title: 'The big money scent'
     }, {
         id: 11,
         rate: 4.12,        
-        image:  '/uploads/images/games/AnubisSecret.jpg',
+        image:  '/casexe-project/images/games/AnubisSecret.jpg',
         title: 'AnubisSecret'
     }, {
         id: 12,
         rate: 3.11,
-        image: '/uploads/images/games/DreamOfKnight.jpg',
+        image: '/casexe-project/images/games/DreamOfKnight.jpg',
         title: 'Dream of Knight'
     }, {
         id: 13,
         rate: 4.72,
-        image: '/uploads/images/games/LuckyMagic.jpg',
+        image: '/casexe-project/images/games/LuckyMagic.jpg',
         title: 'Lucky Magic'
     }, {
         id: 14,
         rate: 4.02,
-        image: '/uploads/images/games/Submarine.jpg',
+        image: '/casexe-project/images/games/Submarine.jpg',
         title: 'Submarine adventure'
     }, {
         id: 15,
         rate: 3.87,
-        image: '/uploads/images/games/MoneyScent.jpg',
+        image: '/casexe-project/images/games/MoneyScent.jpg',
         title: 'The big money scent'
     }, {
         id: 16,
         rate: 4.12,        
-        image:  '/uploads/images/games/AnubisSecret.jpg',
+        image:  '/casexe-project/images/games/AnubisSecret.jpg',
         title: 'AnubisSecret'
     }, {
         id: 17,
         rate: 3.11,
-        image: '/uploads/images/games/DreamOfKnight.jpg',
+        image: '/casexe-project/images/games/DreamOfKnight.jpg',
         title: 'Dream of Knight'
     }, {
         id: 18,
         rate: 4.72,
-        image: '/uploads/images/games/LuckyMagic.jpg',
+        image: '/casexe-project/images/games/LuckyMagic.jpg',
         title: 'Lucky Magic'
     }, {
         id: 19,
         rate: 4.02,
-        image: '/uploads/images/games/Submarine.jpg',
+        image: '/casexe-project/images/games/Submarine.jpg',
         title: 'Submarine adventure'
     }, {
         id: 20,
         rate: 3.87,
-        image: '/uploads/images/games/MoneyScent.jpg',
+        image: '/casexe-project/images/games/MoneyScent.jpg',
         title: 'The big money scent'
     }, {
         id: 21,
         rate: 3.87,
-        image: '/uploads/images/games/MoneyScent.jpg',
+        image: '/casexe-project/images/games/MoneyScent.jpg',
         title: 'The big money scent'
     }
 ]
